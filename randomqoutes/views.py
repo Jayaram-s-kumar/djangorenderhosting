28fad90e450b0dbd9b20349403fc0b5ce10bc4b3
jayaramskumar <kumarjayaram545@gmail.com>
2023-09-06 21:08:04 +0530

    added  bg image

--- randomqoutes/views.py
+++ randomqoutes/views.py
@@ -7,10 +7,27 @@ def home(request):
     api_url = 'https://api.api-ninjas.com/v1/quotes'
     response = requests.get(api_url,headers={'x-api-key':'wEuJMF5D4ghd4bs8fRbpig==oT6NhiuxotmhleMi'})
     if response.status_code == 200:
+
         print(response.text)
-        qoute= json.loads(response.text)[0]['quote']
-        author= json.loads(response.text)[0]['author']
-        print(qoute)
-        return render(request,'quotes.html',{'quote':qoute,'author':author})
+        print(type(response.text),'\n')
+
+        jsondata = json.loads(response.text)
+        print(jsondata)
+        print(type(jsondata),'\n')
+
+        firstelement = jsondata[0]
+        print(firstelement)
+        print(type(firstelement),'\n')
+
+        quote = firstelement['quote']
+        print(quote,'\n')
+        print(type(quote))
+
+        author = json.loads(response.text)[0]['author']
+
+
+
+      
+        return render(request,'quotes.html',{'quote':quote,'author':author})
     else:
         print("Error",response.status_code , response.text)
